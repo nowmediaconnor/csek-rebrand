8662e6dd0864f05966e7b8e1f8ac4e63c706b198
Connor Doman <cdoman@nowmediagroup.ca>
2023-11-06 17:19:19 -0800

    feat(styles): mobileize footer styles

--- footer.php
+++ footer.php
@@ -6,18 +6,18 @@
 
 <canvas id="spline-debug" class="fixed z-[999] left-1/2 top-1/2 w-64 h-64 border border-red-500 -translate-x-1/2 -translate-y-1/2 hidden"></canvas>
 
-<footer id="colophon" class="site-footer relative bg-csek-dark pt-32 pb-12 h-[60vh] font-syne" role="contentinfo">
+<footer id="colophon" class="site-footer relative bg-csek-dark py-4 md:pb-12 font-syne" role="contentinfo">
 	<!-- <?php do_action('csek_rebrand_footer'); ?> -->
-	<div class="grid grid-cols-2 grid-rows-2 gap-y-12 text-csek-light justify-between h-full content-around w-11/12 md:max-w-[75rem] mx-auto">
-		<div class="col-span-2 flex flex-row items-center justify-start gap-8">
-			<h3 class="text-6xl my-12 font-bold">Knock on<br />our door<br />anytime.</h3>
+	<div class="flex flex-col md:grid grid-cols-2 grid-rows-2 gap-y-12 text-csek-light justify-between h-full content-around w-11/12 md:max-w-[75rem] mx-auto">
+		<div class="col-span-2 flex flex-col md:flex-row items-start md:items-center justify-start gap-8">
+			<h3 class="text-5xl md:text-6xl md:my-12 font-bold">Knock on<br />our door<br />anytime.</h3>
 			<a class="lets-talk-open" href="#letstalk">
 				<div class="w-32 h-32 rounded-full bg-csek-red flex flex-row items-center justify-center text-white uppercase font-extrabold text-xs -rotate-[30deg] font-montserrat relative">
 					<span class="absolute">Knock Knock!</span>
 				</div>
 			</a>
 		</div>
-		<div class="self-end flex flex-col gap-8">
+		<div class="md:self-end flex flex-col gap-8 grid-rows-[10rem]">
 			<h3 class="font-syne text-csek-gray font-bold uppercase">Social</h3>
 			<ul class="flex flex-row flex-wrap gap-x-2">
 				<li><a href="#facebook" class="w-10 h-10 border-light border rounded-full flex items-center justify-center" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a></li>
@@ -26,9 +26,9 @@
 				<li><a href="#youtube" class="w-10 h-10 border-light border rounded-full flex items-center justify-center" target="_blank" rel="noopener noreferrer"><i class="fab fa-youtube"></i></a></li>
 			</ul>
 		</div>
-		<div class="self-end justify-self-end flex flex-col items-end gap-4">
-			<img src="/wp-content/themes/csek-rebrand/img/c.svg" alt="Csek Creative letter C monogram" class="w-24" />
-			<div class="flex flex-row h-10 items-center justify-between gap-x-6 flex-wrap text-xs">
+		<div class="col-span-2 md:col-span-1 md:self-end md:justify-self-end flex flex-col md:items-end gap-4">
+			<img src="/wp-content/themes/csek-rebrand/img/c.svg" alt="Csek Creative letter C monogram" class="w-24 hidden md:block" />
+			<div class="flex flex-row h-10 items-start md:items-center justify-start md:justify-between gap-x-6 flex-wrap text-xs">
 				<span>&copy; <?php echo date_i18n('Y'); ?> <?php echo get_bloginfo('name'); ?></span>
 				<span><a href="#privacy">Privacy Policy</a></span>
 				<span><a href="#sitemap">Sitemap</a></span>
