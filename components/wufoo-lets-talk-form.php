39c8a9383e42bc0fe962fd0668b4cfa737e07942
Connor Doman <cdoman@nowmediagroup.ca>
2023-11-27 14:51:22 -0800

    feat(styles): add wufoo styling

--- components/wufoo-lets-talk-form.php
+++ components/wufoo-lets-talk-form.php
@@ -7,7 +7,38 @@
 ?>
 <div id="lets-talk" class="fixed top-0 left-0 w-screen h-screen bg-black z-[200] flex flex-col justify-start items-center text-white overflow-y-scroll py-8 pointer-events-none opacity-0 transition-opacity duration-500 ease-in-out">
     <a id="lets-talk-close" href="#close" class="absolute top-2 right-0 m-8 text-xl font-bold"><i class="fa fa-x"></i></a>
-    <iframe height="962" title="Embedded Wufoo Form" allowtransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none" sandbox="allow-popups-to-escape-sandbox allow-top-navigation allow-scripts allow-popups allow-forms allow-same-origin" src="https://gammatech.wufoo.com/embed/me2cd7i0b7ldia/">
-        <a href="https://gammatech.wufoo.com/forms/me2cd7i0b7ldia/">The form couldn&apos;t load on this page, but you can click here to fill it out!</a>
-    </iframe>
+    <h1 class="font-syne my-12 mx-4 text-7xl font-bold">Ready to chat?</h1>
+
+    <div id="wufoo-me2cd7i0b7ldia"> Fill out my <a href="https://gammatech.wufoo.com/forms/me2cd7i0b7ldia">online form</a>. </div>
+    <script type="text/javascript">
+        var me2cd7i0b7ldia;
+        (function(d, t) {
+            var s = d.createElement(t),
+                options = {
+                    'userName': 'gammatech',
+                    'formHash': 'me2cd7i0b7ldia',
+                    'autoResize': true,
+                    'height': '962',
+                    'async': true,
+                    'host': 'wufoo.com',
+                    'header': 'show',
+                    'ssl': true
+                };
+            s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'secure.wufoo.com/scripts/embed/form.js';
+            s.onload = s.onreadystatechange = function() {
+                var rs = this.readyState;
+                if (rs)
+                    if (rs != 'complete')
+                        if (rs != 'loaded') return;
+                try {
+                    me2cd7i0b7ldia = new WufooForm();
+                    me2cd7i0b7ldia.initialize(options);
+                    me2cd7i0b7ldia.display();
+                } catch (e) {}
+            };
+            var scr = d.getElementsByTagName(t)[0],
+                par = scr.parentNode;
+            par.insertBefore(s, scr);
+        })(document, 'script');
+    </script>
 </div>
