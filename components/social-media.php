a902f3f284edaf3a3c7cb9192e5aeb5717b5118c
Connor Doman <cdoman@nowmediagroup.ca>
2023-12-19 15:42:55 -0800

    tweak(css): socials don't wrap

--- components/social-media.php
+++ components/social-media.php
@@ -8,7 +8,7 @@
 ?>
 <div class="flex flex-col gap-6 grid-rows-[10rem] text-csek-white">
     <h3 class="font-syne text-csek-gray font-bold uppercase leading-none">Social</h3>
-    <ul class="flex flex-row flex-wrap gap-2">
+    <ul class="flex flex-row flex-nowrap gap-2 w-full">
         <li><a href="https://www.instagram.com/csekcreative/" class="w-10 h-10 border-light border rounded-full flex items-center justify-center" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a></li>
         <li><a href="https://twitter.com/csekcreative" class="w-10 h-10 border-light border rounded-full flex items-center justify-center" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-x-twitter"></i></a></li>
         <li><a href="https://www.linkedin.com/showcase/csekcreative/" class="w-10 h-10 border-light border rounded-full flex items-center justify-center" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a></li>
